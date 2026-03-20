64b2f3c2497889b7ce564e130df3b34df3d7020b
Dean Budden <dean@deansPC.lan>
2026-03-19 19:00:51 -0600

    added basic windowing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+int running = 1;
+
 int main(int argc, char* argv[]){
-    printf("hello, INK!\n");
+
+    if(SDL_Init(SDL_INIT_VIDEO) > 1){
+        printf("ERROR! SDL COULD NOT INITIALIZE. SDL ERROR: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* win;
+
+    win = SDL_CreateWindow("game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+
+    if(win == NULL){
+        printf("ERROR! NULL POINTER TO WINDOW THROWN.");
+        return 1;
+    }
+
+    SDL_Renderer* renderer;
+
+    renderer = SDL_CreateRenderer(
+        win,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+
+    if(renderer == NULL){
+        printf("ERROR! NULL POINTER TO RENDERER THROWN.");
+        return 1;
+    }
+
+    SDL_Event e;
+
+    while(running){
+        while(SDL_PollEvent(&e)){
+            switch(e.type){
+                case SDL_QUIT:
+                    running = 0;
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 
     return 0;
 }
